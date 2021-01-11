e1c290481fbb3c8b1407f79892ef198c83b6eba8
miya.clt <miya.clt@gmail.com>
2021-01-11 23:03:33 +0800

    add current user check for edit and update user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,9 @@ module SessionsHelper
     reset_session
     @current_user = nil
   end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
 end
