d7d9bbb559ff4cf5ea9e8784763eed085038df72
miya.clt <miya.clt@gmail.com>
2021-01-11 22:11:26 +0800

    update user profile

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,6 +14,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
+  test "test signup with empty password" do
+    get signup_path
+    assert_no_difference('User.count') do
+      post users_path, params: { user: { name:  "Example User",
+                                          email: "user@example.com",
+                                         password:              "",
+                                         password_confirmation: "" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
