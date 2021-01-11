287818c0ae0a3e3f0d9c3dd8011f0090e0ca19a3
miya.clt <miya.clt@gmail.com>
2021-01-11 22:36:22 +0800

    add login check for edit and update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get signup" do
     get signup_url
     assert_response :success
     assert_select 'title', full_title('Sign up')
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user),params: { user: { name: @user.name,​email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
